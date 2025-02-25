1e6b053ab84669d6125b0f8bc6570d935db27180
1M7md-CS <mohammadhammad464@gmail.com>
2025-02-25 20:26:51 +0300

    Implement InfixToPrefixConverter class

--- src/converter/InfixToPrefixConverter.java
+++ src/converter/InfixToPrefixConverter.java
@@ -1,5 +1,21 @@
 package converter;
 
+import utils.BalancedParentheses;
+
 public class InfixToPrefixConverter {
 
+	public static String convert(String expression) {
+
+		if (!BalancedParentheses.isBalanced(expression)) {
+			throw new IllegalArgumentException("Error: Unbalanced parentheses in expression!");
+		}
+
+		expression = new StringBuilder(expression).reverse().toString();
+
+		expression = expression.replace('(', ' ').replace(')', '(').replace(' ', ')');
+
+		String postfix = InfixToPostfixConverter.convert(expression);
+
+		return new StringBuilder(postfix).reverse().toString();
+	}
 }
