fdc54d110ae0813a8b54ee03276dffb334286096
1M7md-CS <mohammadhammad464@gmail.com>
2025-02-25 19:35:22 +0300

    Implement OperatorHandler class

--- src/utils/OperatorHandler.java
+++ src/utils/OperatorHandler.java
@@ -2,5 +2,25 @@ package utils;
 
 public class OperatorHandler {
 
+	public static boolean isOperator(char element) {
+		return element == '+' || element == '-' || element == '*' || element == '/' || element == '^';
+	}
+
+	public static int applyOperator(int first, char operator, int second) {
+		return switch (operator) {
+			case '+' -> first + second;
+			case '-' -> first - second;
+			case '*' -> first * second;
+			case '/' -> {
+				if (second == 0) {
+					throw new ArithmeticException("Division by zero is not allowed.");
+				}
+				yield first / second;
+			}
+			case '^' -> (int) Math.pow(first, second);
+			default -> throw new IllegalArgumentException("Unknown operator: " + operator);
+		};
+	}
+
 
 }
