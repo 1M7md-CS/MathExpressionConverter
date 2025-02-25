fe1bca59e4a7c9abd9ceadb6ce72780ee7e41c9d
1M7md-CS <mohammadhammad464@gmail.com>
2025-02-25 20:16:38 +0300

    Implement InfixToPostfixConverter class

--- src/converter/InfixToPostfixConverter.java
+++ src/converter/InfixToPostfixConverter.java
@@ -1,5 +1,58 @@
 package converter;
 
+import utils.BalancedParentheses;
+import utils.OperatorHandler;
+
+import java.util.Stack;
+
 public class InfixToPostfixConverter {
+
+	public static String convert(String expression) {
+		if (!BalancedParentheses.isBalanced(expression)) {
+			throw new IllegalArgumentException("Error: Unbalanced parentheses in expression!");
+		}
+
+		for (char ch : expression.toCharArray()) {
+			if (!Character.isDigit(ch) && !OperatorHandler.isOperator(ch) && ch != '(' && ch != ')' && !Character.isWhitespace(ch)) {
+				throw new IllegalArgumentException("Error: Unsupported character '" + ch + "' in expression. Only numbers and operators are allowed.");
+			}
+		}
+
+		Stack<Character> stack = new Stack<>();
+		StringBuilder builder = new StringBuilder();
+		StringBuilder currentNumber = new StringBuilder();
+
+		for (int i = 0; i < expression.length(); i++) {
+			char ch = expression.charAt(i);
+
+			if (Character.isDigit(ch)) {
+				currentNumber.append(ch);
+
+				if (i == expression.length() - 1 || !Character.isDigit(expression.charAt(i + 1))) {
+					builder.append(currentNumber).append(" ");
+					currentNumber.setLength(0);
+				}
+			} else if (ch == '(') {
+				stack.push(ch);
+			} else if (ch == ')') {
+				while (!stack.isEmpty() && stack.peek() != '(') {
+					builder.append(stack.pop()).append(" ");
+				}
+
+				if (!stack.isEmpty()) stack.pop();
+			} else if (OperatorHandler.isOperator(ch)) {
+				while (!stack.isEmpty() && stack.peek() != '(' && OperatorHandler.priority(stack.peek()) >= OperatorHandler.priority(ch)) {
+					builder.append(stack.pop()).append(" ");
+				}
+				stack.push(ch);
+			}
+		}
+
+		while (!stack.isEmpty()) {
+			builder.append(stack.pop()).append(" ");
+		}
+
+		return builder.toString().trim();
+	}
 
 }
