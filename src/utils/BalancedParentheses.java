183dab72a1b97a92212e36417bfdf5751b041d05
1M7md-CS <mohammadhammad464@gmail.com>
2025-02-25 06:38:34 +0300

    Implement BalancedParentheses class to check for balanced parentheses

--- src/utils/BalancedParentheses.java
+++ src/utils/BalancedParentheses.java
@@ -1,9 +1,24 @@
 package utils;
 
+import java.util.Stack;
+
 public class BalancedParentheses {
 
-	public static boolean isBalanced(){
+	public static boolean isBalanced(String expression) {
 
+		Stack<Character> stack = new Stack<>();
+
+		for (char ch : expression.toCharArray()) {
+			if (Character.isDigit(ch) || Character.isAlphabetic(ch) || ch == ' ') continue;
+			if (ch == '(') {
+				stack.push(ch);
+			} else if (ch == ')') {
+				if (stack.isEmpty()) {
+					return false;
+				}
+				stack.pop();
+			}
+		}
+		return stack.isEmpty();
 	}
-
 }
