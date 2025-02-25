29a303bca092663046ac3adea22d99168a8f5016
1M7md-CS <mohammadhammad464@gmail.com>
2025-02-25 22:28:51 +0300

    Added Checker class to centralize validation and reduce redundancy.

---
+++ src/utils/Checker.java
@@ -0,0 +1,19 @@
+package utils;
+
+public class Checker {
+
+	public static void checkBalancedParentheses(String expression) {
+		if (!BalancedParentheses.isBalanced(expression)) {
+			throw new IllegalArgumentException("Error: Unbalanced parentheses in expression!");
+		}
+	}
+
+	public static void checkValidCharacters(String expression) {
+		for (char ch : expression.toCharArray()) {
+			if (!Character.isDigit(ch) && !OperatorHandler.isOperator(ch)
+					&& ch != '(' && ch != ')' && !Character.isWhitespace(ch)) {
+				throw new IllegalArgumentException("Error: Unsupported character '" + ch + "' in expression. Only numbers and operators are allowed.");
+			}
+		}
+	}
+}
